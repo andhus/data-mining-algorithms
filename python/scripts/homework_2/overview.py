f0383c1a919d91bba04a0d96b905b19978dd0c0d
Anders Huss <andhus@kth.se>
2018-11-18 10:32:37 +0100

    break out argsparsing

--- python/scripts/homework_2/overview.py
+++ python/scripts/homework_2/overview.py
@@ -5,44 +5,16 @@ from pprint import pprint
 
 from data_mining.data import get_frequent_itemset
 from data_mining.frequency import get_frequent_sets_counts, get_rules
-
-
-def get_argument_parser(
-    num_baskets_limit=None,
-    max_set_size=None,
-    min_support=0.005,
-    min_confidence=0.10
-):
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--num-baskets-limit',
-        type=int,
-        default=num_baskets_limit,
-        help='the number of (first n) baskets to analyse, leave empty for all')
-    parser.add_argument(
-        '--max-set-size',
-        type=int,
-        default=max_set_size,
-        help='max size of frequent sets to identify, leave empty for all'
-    )
-    parser.add_argument(
-        '--min-support',
-        type=float,
-        default=min_support,
-        help='minimum support of sets and rules to identify'
-    )
-    parser.add_argument(
-        '--min-confidence',
-        type=float,
-        default=min_confidence,
-        help='minimum confidence for rules to identify'
-    )
-
-    return parser
+from data_mining.script_utils import get_frequent_itemsets_argument_parser
 
 
 if __name__ == '__main__':
-    parser = get_argument_parser()
+    parser = get_frequent_itemsets_argument_parser(
+        num_baskets_limit=None,
+        max_set_size=None,
+        min_support=0.005,
+        min_confidence=0.10
+    )
     args = parser.parse_args()
     print(args)
     baskets = get_frequent_itemset(first_n=args.num_baskets_limit)
